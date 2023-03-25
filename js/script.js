fd648efc54381fa91f542a29782d4fd67cfecdb2
Fernando Futila <fernando.futila@gmail.com>
2023-03-25 13:43:58 -0300

    Parâmetros URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,17 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativar Items do Orçamento;
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
